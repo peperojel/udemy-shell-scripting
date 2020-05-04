c6ea2823653440dc73f73f98a384e44b2ce85945
peperojel <joserojel9@gmail.com>
2020-05-04 00:51:05 -0400

    First commit

---
+++ ex3_add-new-local-user.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+# Refactor of add-local-user.sh
+# Permite la creación de usuario indicando id y comentario como argumentos en la ejecución del script
+# Se le asigna una contraseña segura de forma automática
+
+# Verificar si el usuario tiene privilegios de root
+if [[ "${UID}" -ne 0 ]]
+then
+    echo "You don't have root privileges..."
+    exit 1
+fi
+
+# Verificar que el usuario proveyó al menos un argumento
+if [[ "${#}" -lt 1 ]]
+then
+  echo "Usage: ${0} USER_NAME [COMMENT]..."
+  echo "Create an account on the local system with the name of USER_NAME and a comments field of COMMENT."
+  exit 1
+fi
+
+# Asignar nombre de usuario y comentario (if exists)
+USER_NAME="${1}"
+shift
+
+# if [[ "${#}" -ge 1 ]]
+# then
+#   COMMENT="${1}"
+#   shift
+#   while [[ "${#}" -gt 0 ]]
+#   do
+#     COMMENT+=" ${1}"
+#     shift
+#   done    
+# fi
+COMMENT="${@}"
+
+# Creación del nuevo usuario
+useradd -c "${COMMENT}" -m ${USER_NAME}
+
+# Check for return of previous command
+if [[ "${?}" -ne 0 ]]
+then
+    echo "Failed useradd execution"
+    exit 1
+fi
+
+# Generación de una password aleatoria segura
+PASSWORD=$(date +%s%N | sha256sum | head -c48)
+PASSWORD+=$(echo "#$%&/()=?_!" | fold -w1 | shuf | head -c1)
+
+# Establecer password a usuario
+echo ${PASSWORD} | passwd --stdin ${USER_NAME}
+
+# Verificar que el cambio de contraseña fue exitoso
+if [[ "${?}" -ne 0 ]]
+then
+  echo "Failed setting the password"
+  exit 1
+fi
+
+# Forzar a que el usuario cambie su contraseña cuando ingrese
+passwd -e ${USER_NAME}
+
+# Imprimir los resultados
+echo "
+username:
+${USER_NAME}
+
+password:
+${PASSWORD}
+
+host:
+$(hostname)"
+
+exit 0
